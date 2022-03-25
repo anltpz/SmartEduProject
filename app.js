d292cd2efb13b87be1da3880ead59d0f224517ef
Anıl TOPUZ <aniltopuz@gmail.com>
2022-03-25 13:17:27 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 
+
+//template ejs
+app.set('view engine', 'ejs');
+
+//middleware
+app.use(express.static('public'));
+
+
+//routes
+
+app.get('/', (req, res) => {
+    res.status(200).render('index',{page_name:"index"});
+}
+);
+app.get('/about', (req, res) => {
+    res.status(200).render('about',{page_name:"about"});
+}
+);
+
+
+
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 }
 );
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-}
-);
